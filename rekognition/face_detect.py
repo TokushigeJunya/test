935297af79e6d57ea3dc58f307ca0ad08e0e353f
TokushigeJunya <156087023+TokushigeJunya@users.noreply.github.com>
2025-04-16 23:07:17 +0900

    Delete rekognition directory

--- rekognition/face_detect.py
+++
@@ -1,8 +0,0 @@
-import boto3
-import json
-import sys
-
-rekognition = boto3.client('rekognition')
-with open(sys.argv[1], 'rb') as file:
-    result = rekognition.detect_faces(Image={'Bytes': file.read()})
-    print(json.dumps(result, indent=4))
